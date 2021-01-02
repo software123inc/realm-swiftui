9dc1610c3677751b301d329d81102266b883bc0b
Tim Newton <tim.newton@software123.com>
2021-01-02 09:51:13 -0800

    Initial Commit

---
+++ realm-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  realm-swiftui
+//
+//  Created by Tim W. Newton on 1/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ realm-swiftui/realm_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  realm_swiftuiApp.swift
+//  realm-swiftui
+//
+//  Created by Tim W. Newton on 1/2/21.
+//
+
+import SwiftUI
+
+@main
+struct realm_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
